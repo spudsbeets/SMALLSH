c5eff82a57486f54fd15ecf7cc005661436012c2
Sean Miller <111613875+spudsbeets@users.noreply.github.com>
2026-02-13 18:00:59 +0000

    added built in exit and cd functionality

--- milles29_assignment4.c
+++ milles29_assignment4.c
@@ -7,11 +7,17 @@ Citations:
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <signal.h>
 
 #define INPUT_LENGTH 2048
 #define MAX_ARGS 512
+#define MAX_CHILDREN 25
 
 /* Citation #1 */
+/* Struct to store command line information. */
 struct command_line {
 	char *argv[MAX_ARGS + 1];
 	int argc;
@@ -20,7 +26,12 @@ struct command_line {
 	bool is_bg;
 };
 
+/* Setup array for children pids for exit process. */
+pid_t children_pids[MAX_CHILDREN];
+int child_count = 0;
+
 /* Citation #1 */
+/* Parse command lines. */
 struct command_line *parse_input() {
     char input[INPUT_LENGTH];
     struct command_line *curr_command = (struct command_line *) calloc(1, sizeof(struct command_line));
@@ -45,14 +56,69 @@ struct command_line *parse_input() {
     return curr_command;
 }
 
+/* Built in exit function for shell. */
+void builtin_exit() {
+    for (int i = 0; i < child_count; i++) {
+        kill(children_pids[i], SIGTERM);
+    }
+
+    for (int i = 0; i < child_count; i++) {
+        waitpid(children_pids[i], NULL, 0);
+    }
+
+    exit(0);
+}
+
+void builtin_cd(struct command_line *curr_command) {
+    char *path;
+
+    if (curr_command->argc > 1) {
+        path = curr_command->argv[1];
+    } else {
+        path = getenv("HOME");
+        if (path == NULL) {
+            fprintf(stderr, "HOME not set.\n");
+            return;
+        }
+    }
+
+    if (chdir(path) == -1) {
+        perror("cd");
+    }
+}
+
+
 int main() {
+    printf("$ smallsh\n");
+
     /* Citation #1 */
     struct command_line *curr_command;
 
-    printf("$ smallsh\n");
+    /* pid_t sampleSpawn = fork();
+
+    switch(sampleSpawn) {
+        case -1:  
+            perror("fork() failure!");
+            exit(1);
+            break;
+        case 0:
+            sleep(30);
+            exit(0);
+        default:
+            children_pids[child_count++] = sampleSpawn; */
 
     while (true) {
         curr_command = parse_input();
+
+        if (curr_command->argc == 0 || strncmp(curr_command->argv[0], "#", 1) == 0) {
+            continue;
+        } 
+        if (strcmp(curr_command->argv[0], "exit") == 0) {
+            builtin_exit();
+        }
+        if (strcmp(curr_command->argv[0], "cd") == 0) {
+            builtin_cd(curr_command);
+        } 
     }
 
     return EXIT_SUCCESS;
